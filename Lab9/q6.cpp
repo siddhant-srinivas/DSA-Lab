49698d90de99558e59d0008c1260a87c12f29adf
siddhant-srinivas <117804794+siddhant-srinivas@users.noreply.github.com>
2023-04-17 19:17:57 +0530

    Fixed q6

--- Lab9/q6.cpp
+++ Lab9/q6.cpp
@@ -1,35 +1,28 @@
-#include <iostream>
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
+int main() 
 {
-    int n, elem, diff, min = 9999999;
-    cin >> n >> diff;
-    priority_queue <int> q1;
-    priority_queue <int> q2;
-    for(int i = 0; i<n; i++)
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) 
     {
-        cin >> elem;
-        q1.push(elem);
-        if(elem < min) min = elem;
+        cin >> a[i];
     }
-    int required = min + diff;
-    while(!q1.empty() && q1.top() >= required)
+    sort(a.begin(), a.end());
+    int i = 0, j = 1, ans = 0;
+    while (j < n) 
     {
-        q2.push(q1.top());
-        q1.pop();
-    }
-    int count = 0;
-    while(!q1.empty() && !q2.empty())
-    {
-        if(q2.top() - q1.top() >= diff)
+        if (a[j] - a[i] >= k) 
         {
-            count++;
-            q2.pop();
-        }
-        q1.pop();
-    }  
-    cout << count << endl;
+            ans++;
+            i++;
+            j++;
+        } 
+        else 
+            j++;
+    }
+    cout << ans << endl;
+    return 0;
 }
