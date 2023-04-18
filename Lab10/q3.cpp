2010565ae7e52d5d186ac85b532e4578cadbd0b6
siddhant-srinivas <117804794+siddhant-srinivas@users.noreply.github.com>
2023-04-19 04:26:58 +0530

    took q3 from Arun

--- Lab10/q3.cpp
+++ Lab10/q3.cpp
@@ -1,75 +1,62 @@
 #include <iostream>
-using namespace std;
- 
-int find_median(int *a, int n, int *b, int m)
-{
-    if(n > m)
-        return find_median(b, m, a, n);
+#include <vector>
 
-    int partition1, partition2, median;
-    int l = 0, r = n;
-     
-    while (l <= r)
+using namespace std;
+
+float median_of_two_sorted_arrays(vector<int> arr1, vector<int> arr2)
+{
+    int n = arr1.size();
+    int m = arr2.size();
+
+    if (n > m)
+        return median_of_two_sorted_arrays(arr2, arr1);
+
+    int low = 0, high = n;
+
+    while (low <= high)
     {
-        partition1 = (l + r) / 2;
-        partition2 = ((n + m + 1) / 2) - partition1;
-         
-        if (partition2 < 0)
+        int cut1 = (low + high) / 2;
+        int cut2 = (n + m + 1) / 2 - cut1;
+
+        int left1 = (cut1 == 0) ? INT_MIN : arr1[cut1 - 1];
+        int left2 = (cut2 == 0) ? INT_MIN : arr2[cut2 - 1];
+        int right1 = (cut1 == n) ? INT_MAX : arr1[cut1];
+        int right2 = (cut2 == m) ? INT_MAX : arr2[cut2];
+
+        if (left1 <= right2 && left2 <= right1)
         {
-            r = partition1-1;       // Partition 2 not possible so partition1 elements only
-            continue;
+            if ((n + m) % 2 == 0)
+                return (max(left1, left2) + min(right1, right2)) / 2.0;
+            else
+                return max(left1, left2);
         }
-                // If partit1 = n, no elems in second half from a; If partit2 = 0, similarly for first half and b
-        if (partition1 < n && partition2 > 0 && b[partition2 - 1] > a[partition1])       
-            l = partition1 + 1;
-                 
-                // Similarly but first half and second half
-        else if (partition1 > 0 && partition2 < m && b[partition2] < a[partition1 - 1])       
-            r = partition1 - 1;
- 
+        else if (left1 > right2)
+            high = cut1 - 1;
         else
-        {
-            // No elements in first half from a so we return last element in b from the first half
-            if (partition1 == 0)          
-                median = b[partition2 - 1];           
-             
-            // No elements in first half from b so we return last element in a from the first half
-            else if (partition2 == 0)           
-                median = a[partition1 - 1];           
-            else           
-                median = max(a[partition1 - 1], b[partition2 - 1]);           
-            break;
-        }
+            low = cut1 + 1;
     }
-     
-    if ((n + m) % 2 == 1)
-        return median;
-         
-            // No elements from a in second half 
-    if (partition1 == n)
-        return (median+b[partition2]) / 2;
-         
-            // No Elements from b in second half 
-    if (partition2 == m)
-        return (median + a[partition1]) / 2;
-     
-    return (median + min(a[partition1], b[partition2])) / 2;
 }
 
 int main()
 {
     int n, m;
+    vector<int> arr1, arr2;
+
     cin >> n;
-    int a[n];
-    for(int i= 0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        int x;
+        cin >> x;
+        arr1.push_back(x);
     }
     cin >> m;
-    int b[m];
-    for(int i = 0; i<m; i++)
+    for (int i = 0; i < m; i++)
     {
-        cin >> b[i];
+        int x;
+        cin >> x;
+        arr2.push_back(x);
     }
-    cout << find_median(a, n, b, m) << endl;
+    float median = median_of_two_sorted_arrays(arr1, arr2);
+    cout << median << endl;
+    return 0;
 }
