9fe08e38aa8a48bac6cd3010d7ea44adb34b0346
Siddhant Srinivas <117804794+siddhant-srinivas@users.noreply.github.com>
2023-04-26 13:50:53 +0530

    Update q5.cpp

--- lab11/q5.cpp
+++ lab11/q5.cpp
@@ -1,48 +1,38 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
-
 using namespace std;
 
 int t[502][502];
 
-int solve(int arr[],int i, int j)
+int solve(int arr[], int i, int j)
 {
     if(i >= j)
-    {
         return 0;
-    }
-    if(t[i][j]!=-1)
-    {
+
+    if(t[i][j] != -1)
         return t[i][j];
-    }
 
-    int mini=INT_MAX;
-    for(int k = i;k < j;k++)
+    int min = INT_MAX;
+
+    for(int k = i; k < j; k++)
     {
-        int temp_ans = solve(arr,i,k) + solve(arr,k+1,j) + (arr[i-1] * arr[k] * arr[j]);
-        if(temp_ans < mini)
-            mini = temp_ans;
+        int temp = solve(arr, i, k) + solve(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
+        if(temp < min)
+            min = temp;
     }
-    return t[i][j]=mini;
-} 
-
+    return t[i][j] = min;
+}
 
 int main()
 {
     int n;
-    cin>>n;
-    int arr[n+1];
+    cin >> n;
+    int arr[n + 1];
     for(int i = 0; i < n + 1; i++)
     {
         cin >> arr[i];
     }
     memset(t, -1, sizeof(t));
-    cout << solve(arr,1,n);
+    cout << solve(arr, 1, n) << endl;
+    return 0;
 }
-
-
-
-
-
-
