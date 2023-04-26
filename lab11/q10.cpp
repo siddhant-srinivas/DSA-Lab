35f524d0381fbe3f6721e75e435964131833def0
Siddhant Srinivas <117804794+siddhant-srinivas@users.noreply.github.com>
2023-04-26 13:45:43 +0530

    Update q10.cpp

--- lab11/q10.cpp
+++ lab11/q10.cpp
@@ -1,37 +1,36 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int n,k;
-    cin>>n>>k;
+    int n, k;
+    cin >> n >> k;
     int arr[n];
-    for(int i=0;i<n;i++)
+    for(int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
 
     bool t[n+1][k+1];
-    for(int i=0;i<=n;i++)
+    for(int i = 0;i <= n;i++)
     {
-        for(int j=0;j<=k;j++)
+        for(int j = 0;j <= k; j++)
         {
-            if(i==0)
-                t[i][j]=false;
+            if(i == 0)
+                t[i][j] = false;
         }
     }
 
-    for(int i=0;i<=n;i++)
+    for(int i = 0;i <= n;i++)
     {
-        for(int j=0;j<=k;j++)
+        for(int j = 0;j <= k; j++)
         {
-            if(j==0)
-                t[i][j]=true;
+            if(j == 0)
+                t[i][j] = true;
         }
     }
-    for(int i=1;i<=n;i++)
+    for(int i = 1;i < n + 1;i++)
     {
-        for(int j=1;j<=k;j++)
+        for(int j = 1;j < k + 1;j++)
         {
             if(arr[i-1]<=j)
                 t[i][j]=(t[i-1][j-arr[i-1]]||t[i-1][j]);
